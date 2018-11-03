c2c5e7c7290c40abb6a81bb837ddcb830d48811b
Jefferson Matheus <jefferson227@gmail.com>
2018-11-02 22:57:26 -0300

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Testing from './Components/Testing';
+import AnotherTesting from './Components/AnotherTesting';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Testing />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact={true} component={Testing} />
+            <Route path="/another" exact={true} component={AnotherTesting} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
