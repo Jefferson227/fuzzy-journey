d487b18216192a4345c89963aecde16e62bf80f6
Jefferson Matheus <jefferson227@gmail.com>
2018-11-04 03:18:52 -0300

    Add axios
    Trying to get data using fetch or axios
    to the Estoque page
    
    Throwing error on getting the data

--- src/Pages/Estoque.js
+++ src/Pages/Estoque.js
@@ -2,26 +2,42 @@ import React from 'react';
 import { Field, Input, Control, Container, Box } from 'bloomer';
 import Header from '../Components/Header/Header';
 import FloatingButton from '../Components/FloatingButton/FloatingButton';
+// import axios from 'axios';
 
-const Estoque = () => {
-  return (
-    <div>
-      <Header />
-      <Container style={{ padding: 10 }}>
-        <Field>
-          <Control>
-            <Input type="text" isColor="success" placeholder="Digite o nome do produto" />
-          </Control>
-        </Field>
-        <Box>Item 1</Box>
-        <Box>Item 2</Box>
-        <Box>Item 3</Box>
-        <Box>Item 4</Box>
-        <Box>Item 5</Box>
-      </Container>
-      <FloatingButton />
-    </div>
-  );
+export default class Estoque extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    console.log('test');
+
+    fetch('http://localhost:3000/assets/data/estoque.json')
+      .then(res => res.json())
+      .then(res => {
+        const products = res;
+        this.setState({ products });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container style={{ padding: 10 }}>
+          <Field>
+            <Control>
+              <Input type="text" isColor="success" placeholder="Digite o nome do produto" />
+            </Control>
+          </Field>
+          { this.state.products.map(product => <Box>{ product.productName }</Box>) }
+        </Container>
+        <FloatingButton />
+      </div>
+    );
+  }
 };
-
-export default Estoque;
