6898af3e9a17f20d0314a7ac68daf2e3030a25ab
Jefferson Matheus <jefferson227@gmail.com>
2018-11-09 22:32:44 -0300

    Make the mock json public

--- src/Pages/Estoque.js
+++ src/Pages/Estoque.js
@@ -31,7 +31,7 @@ export default class Estoque extends React.Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:3000/assets/data/estoque.json')
+    fetch('/assets/data/estoque.json')
       .then(res => res.json())
       .then(res => {
         const products = res;
